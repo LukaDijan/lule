df3dfffb93a156f38eb20e0ad029ba8b29c7954d
Luka Dijan <ldijan00@fesb.hr>
2021-08-18 12:28:00 +0200

    Add linters and pre-commit config

--- lule/api/app.py
+++ lule/api/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 from ..database.models import db
 
+
 def get_app():
     app = FastAPI(title="lule API")
     db.init_app(app)
     return app
+
 
 application = get_app()
--- lule/settings.py
+++ lule/settings.py
@@ -1,4 +1,3 @@
-from starlette.config import Config
 from envparse import env
 
 env.read_envfile()
