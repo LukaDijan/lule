c39f5cebe0c5c11c3320a4834dbebc2f622a21cc
Luka Dijan <ldijan00@fesb.hr>
2021-11-02 17:08:36 +0100

    POST /institutions 401 Unathorized

--- lule/api/security.py
+++ lule/api/security.py
@@ -11,4 +11,4 @@ async def get_api_key(api_key_header: str = Security(api_key_header)):
     if api_key_header == API_KEY:
         return api_key_header
     else:
-        raise HTTPException(status.HTTP_403_FORBIDDEN)
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
