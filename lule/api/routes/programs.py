38338956d8781f0b484aa29a3f2d47c39b15bd88
Luka Dijan <ldijan00@fesb.hr>
2021-11-11 23:50:05 +0100

    Added whole security

--- lule/api/routes/programs.py
+++ lule/api/routes/programs.py
@@ -1,10 +1,12 @@
 from uuid import uuid4
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security.api_key import APIKey
 
 from ...database.controller.institutions import get_institution_by_name
 from ...database.controller.programs import store_program
 from ..schemas.programs import ProgramCreationRequest
+from ..security import get_api_key
 
 router = APIRouter(
     responses={
@@ -14,7 +16,9 @@ router = APIRouter(
 
 
 @router.post("/institutions/{institution_id}/programs", tags=["Programs"])
-async def create_program(program: ProgramCreationRequest):
+async def create_program(
+    program: ProgramCreationRequest, api_key: APIKey = Depends(get_api_key)
+):
     if institution := await get_institution_by_name(program.institution):
         Program = await store_program(uuid4(), program.name, institution.id)
         # await Program.update(college_id = institution.id).apply()
